458693fa5b427ba86da3eee7dd03ba61192c9c86
Murilo Henderson <acmurilohenderson@gmail.com>
2025-09-27 19:54:05 -0300

    Finalização do ex005

--- ex005/index.php
+++ ex005/index.php
@@ -8,6 +8,10 @@
 </head>
 <body>
     <main>
+        <?php 
+            $salario = $_GET["salario"] ?? 0;
+            $salarioMinimo = 1518;
+        ?>
         <h1>Salário mínimo</h1>
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="salario">Salario</label>
@@ -16,6 +20,11 @@
         </form>
         <section id="resultado">
             <h2>Resultado</h2>
+            <?php 
+                $resultadoSalarios = intdiv($salario, $salarioMinimo);
+                $resultadoResto = $salario % $salarioMinimo;
+                echo "<p>Quem recebe um salário de R$".number_format($salario, 2, ",", ".")." ganha $resultadoSalarios salários mínimos + R$".number_format($resultadoResto, 2, ",", ".")."</p>";
+            ?>
         </section>
     </main>
 </body>
